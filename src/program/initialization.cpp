0a2364313c7f9f23d3c058aeb416afbe5214b263
QtLittleXu <QtLittlXu@163.com>
2024-01-08 22:16:12 +0800

    Feat(program): Finish the implementation of executable_path() on WIndows

--- src/program/initialization.cpp
+++ src/program/initialization.cpp
@@ -1,20 +1,15 @@
 #include "program/initialization.h"
-#include <unistd.h>
 #include <vector>
 
 namespace li::program 
 {
 
 
-#ifdef __WIN32
+// Only support executable_path() on Windows and Linux now
 
-filesystem::path executable_path()
-{
-	// To do
-	return "";
-}
+#ifdef __WIN32__
 
-#elif __linux__
+#include <windows.h>
 
 filesystem::path executable_path()
 {
@@ -22,6 +17,51 @@ filesystem::path executable_path()
 	std::size_t size = buf.size();
 	bool havePath = false;
 	bool shouldContinue = true;
+
+	do
+	{
+		DWORD result = GetModuleFileNameA(nullptr, buf.data(), size);
+		DWORD lastError = GetLastError();
+		if (result == 0)
+		{
+			shouldContinue = false;
+		}
+		else if (result < size)
+		{
+			havePath = true;
+			shouldContinue = false;
+		}
+		else if (result == size && (lastError == ERROR_INSUFFICIENT_BUFFER || lastError == ERROR_SUCCESS))
+		{
+			size *= 2;
+			buf.resize(size);
+		}
+		else
+		{
+			shouldContinue = false;
+		}
+	} while (shouldContinue);
+
+	if (!havePath)
+	{
+		return "";
+	}
+
+	std::string path = buf.data();
+	return path;
+}
+
+#elif __linux__
+
+#include <unistd.h>
+
+filesystem::path executable_path()
+{
+	std::vector<char> buf(1024, 0);
+	std::size_t size = buf.size();
+	bool havePath = false;
+	bool shouldContinue = true;
+
 	do
 	{
 		ssize_t result = readlink("/proc/self/exe", buf.data(), size);
